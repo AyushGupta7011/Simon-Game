2bfc7432f6e74faab8541a0ea60eefda8bacacf3
Ayush <guptaayush630@gmail.com>
2020-09-22 19:56:48 +0530

    Checking the User's Answer against the game sequence

--- game.js
+++ game.js
@@ -23,11 +23,36 @@ $(".btn").click(function() {
   var userChosenColour = $(this).attr("id");
   userClickedPattern.push(userChosenColour);
 
-  playSound(userChosenColour);
+  playSound(userChosenColour);  
+  animatePress(userChosenColour);
 
+  checkAnswer(userClickedPattern.length-1);
 });
 
+function checkAnswer(currentLevel) {
+
+  if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
+
+    console.log("success");
+
+    if (userClickedPattern.length === gamePattern.length){
+
+        setTimeout(function () {
+        nextSequence();
+      }, 1000);
+
+    }
+
+  } else {
+
+    console.log("wrong");
+
+  }
+
+}
+
 function nextSequence() {
+  userClickedPattern = [];
 
   level++;
 
